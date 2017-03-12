e7fb6a928d2c4a4f553227f57e85d8323228465b
AramisAraujo <aramis.araujo@ccc.ufcg.edu.br>
2017-03-12 17:12:05 -0300

    Refatora a classe anuncio para seguir o padrao builder

--- src/main/java/br/edu/ufcg/computacao/si1/model/Anuncio.java
+++ src/main/java/br/edu/ufcg/computacao/si1/model/Anuncio.java
@@ -9,144 +9,198 @@ import java.util.Date;
  * Created by Marcus Oliveira on 08/12/16.
  */
 @Entity
-@Table(name="tb_anuncio")
+@Table(name = "tb_anuncio")
 public class Anuncio {
 
-    private static final String[] tipos = new String[] {"movel", "imovel", "emprego"};
+	private static final String[] tipos = new String[] { "movel", "imovel", "emprego" };
 
+	private final static DateFormat DATE_FORMAT = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss");
 
-    private final static DateFormat DATE_FORMAT = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss");
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "_id", nullable = false, unique = true)
+	private Long _id;
 
-    @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    @Column(name = "_id", nullable = false, unique = true)
-    private Long _id;
+	@Column(name = "titulo", nullable = false)
+	private String titulo;
 
-    @Column(name = "titulo", nullable = false)
-    private String titulo;
+	@Column(name = "data_criacao", nullable = false)
+	private Date dataDeCriacao;
 
-    @Column(name = "data_criacao", nullable = false)
-    private Date dataDeCriacao;
+	@Column(name = "preco", nullable = false)
+	private double preco;
 
-    @Column(name = "preco", nullable = false)
-    private double preco;
+	@Column(name = "nota")
+	private String nota;
 
-    @Column(name = "nota")
-    private String nota;
+	@Column(name = "tipo", nullable = false)
+	private String tipo;
 
-    @Column(name = "tipo", nullable = false)
-    private String tipo;
+	public Anuncio(String titulo, Date dataDeCriacao, double preco, String nota, String tipo) {
+		this.titulo = titulo;
+		this.dataDeCriacao = dataDeCriacao;
+		this.preco = preco;
+		this.nota = nota;
+		this.tipo = tipo;
+	}
+	
+	private Anuncio(AnuncioBuilder builder) {
+		this.titulo = builder.titulo;
+		this.dataDeCriacao = builder.dataDeCriacao;
+		this.preco = builder.preco;
+		this.nota = builder.nota;
+		this.tipo = builder.tipo;
+	}
+	
+	/**
+	 * Requerimento do JPA
+	 */
+	Anuncio(){
+		
+	}
 
-    public Anuncio(String titulo, Date dataDeCriacao, double preco, String nota, String tipo) {
-        this.titulo = titulo;
-        this.dataDeCriacao = dataDeCriacao;
-        this.preco = preco;
-        this.nota = nota;
-        this.tipo = tipo;
-    }
+	/**
+	 * Retorna o id do anuncio
+	 * 
+	 * @return o id do anuncio
+	 */
+	public Long get_id() {
+		return _id;
+	}
 
-    public Anuncio() {
-        titulo = "";
-        dataDeCriacao = new Date();
-        preco = 0;
-        nota = "";
-        tipo = "";
-    }
+	/**
+	 * Modifica o id do anuncio
+	 * 
+	 * @param _id
+	 *            id a ser colocado no anuncio
+	 */
+	public void set_id(Long _id) {
+		this._id = _id;
+	}
 
-    /**
-     * Retorna o id do anuncio
-     * @return o id do anuncio
-     */
-    public Long get_id() {
-        return _id;
-    }
+	public String getTitulo() {
+		return titulo;
+	}
 
-    /**
-     * Modifica o id do anuncio
-     * @param _id id a ser colocado no anuncio
-     */public void set_id(Long _id) {
-        this._id = _id;
-    }
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
 
-    public String getTitulo() {
-        return titulo;
-    }
+	public String getDataDeCriacao() {
+		return DATE_FORMAT.format(dataDeCriacao);
+	}
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
+	public void setDataDeCriacao(Date dataDeCriacao) {
+		this.dataDeCriacao = dataDeCriacao;
+	}
 
-    public String getDataDeCriacao() {
-        return DATE_FORMAT.format(dataDeCriacao);
-    }
+	public double getPreco() {
+		return preco;
+	}
 
-    public void setDataDeCriacao(Date dataDeCriacao) {
-        this.dataDeCriacao = dataDeCriacao;
-    }
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
 
-    public double getPreco() {
-        return preco;
-    }
+	public String getNota() {
+		return nota;
+	}
 
-    public void setPreco(double preco) {
-        this.preco = preco;
-    }
+	public void setNota(String nota) {
+		this.nota = nota;
+	}
 
-    public String getNota() {
-        return nota;
-    }
+	public String getTipo() {
+		return tipo;
+	}
 
-    public void setNota(String nota) {
-        this.nota = nota;
-    }
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	public static class AnuncioBuilder {
+		@Id
+		@GeneratedValue(strategy = GenerationType.AUTO)
+		@Column(name = "_id", nullable = false, unique = true)
+		private Long _id;
 
-    public String getTipo() {
-        return tipo;
-    }
+		@Column(name = "titulo", nullable = false)
+		private String titulo;
 
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
+		@Column(name = "data_criacao", nullable = false)
+		private Date dataDeCriacao = new Date();
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Anuncio)) return false;
+		@Column(name = "preco", nullable = false)
+		private double preco;
 
-        Anuncio anuncio = (Anuncio) o;
+		@Column(name = "nota")
+		private String nota = "";
 
-        if (Double.compare(anuncio.getPreco(), getPreco()) != 0) return false;
-        if (!get_id().equals(anuncio.get_id())) return false;
-        if (!getTitulo().equals(anuncio.getTitulo())) return false;
-        if (!getDataDeCriacao().equals(anuncio.getDataDeCriacao())) return false;
-        if (getNota() != null ? !getNota().equals(anuncio.getNota()) : anuncio.getNota() != null) return false;
-        return getTipo().equals(anuncio.getTipo());
+		@Column(name = "tipo", nullable = false)
+		private String tipo;
 
-    }
+	    public AnuncioBuilder(String titulo, double preco, String tipo) {
+	      this.titulo = titulo;
+	      this.preco = preco;
+	      this.tipo = tipo;
+	    }
 
-    @Override
-    public int hashCode() {
-        int result;
-        long temp;
-        result = get_id().hashCode();
-        result = 31 * result + getTitulo().hashCode();
-        result = 31 * result + getDataDeCriacao().hashCode();
-        temp = Double.doubleToLongBits(getPreco());
-        result = 31 * result + (int) (temp ^ (temp >>> 32));
-        result = 31 * result + (getNota() != null ? getNota().hashCode() : 0);
-        result = 31 * result + getTipo().hashCode();
-        return result;
-    }
+	    public AnuncioBuilder nota(String  nota) {
+	      this.nota = nota;
+	      return this;
+	    }
 
-    @Override
-    public String toString() {
-        return "Anuncio{" +
-                "_id=" + _id +
-                ", titulo='" + titulo + '\'' +
-                ", dataDeCriacao=" + getDataDeCriacao() +
-                ", preco=" + preco +
-                ", nota=" + nota +
-                ", tipo='" + tipo + '\'' +
-                '}';
-    }
+	    public AnuncioBuilder dataDeCriacao(Date dataDeCriacao) {
+	      this.dataDeCriacao = dataDeCriacao;
+	      return this;
+	    }
+
+	    public Anuncio build() {
+	      return new Anuncio(this);
+	    }
+
+	  }
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (!(o instanceof Anuncio))
+			return false;
+
+		Anuncio anuncio = (Anuncio) o;
+
+		if (Double.compare(anuncio.getPreco(), getPreco()) != 0)
+			return false;
+		if (!get_id().equals(anuncio.get_id()))
+			return false;
+		if (!getTitulo().equals(anuncio.getTitulo()))
+			return false;
+		if (!getDataDeCriacao().equals(anuncio.getDataDeCriacao()))
+			return false;
+		if (getNota() != null ? !getNota().equals(anuncio.getNota()) : anuncio.getNota() != null)
+			return false;
+		return getTipo().equals(anuncio.getTipo());
+
+	}
+
+	@Override
+	public int hashCode() {
+		int result;
+		long temp;
+		result = get_id().hashCode();
+		result = 31 * result + getTitulo().hashCode();
+		result = 31 * result + getDataDeCriacao().hashCode();
+		temp = Double.doubleToLongBits(getPreco());
+		result = 31 * result + (int) (temp ^ (temp >>> 32));
+		result = 31 * result + (getNota() != null ? getNota().hashCode() : 0);
+		result = 31 * result + getTipo().hashCode();
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return "Anuncio{" + "_id=" + _id + ", titulo='" + titulo + '\'' + ", dataDeCriacao=" + getDataDeCriacao()
+				+ ", preco=" + preco + ", nota=" + nota + ", tipo='" + tipo + '\'' + '}';
+	}
 }
